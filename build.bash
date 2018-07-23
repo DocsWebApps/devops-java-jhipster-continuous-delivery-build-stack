bdeb3a64377aa78ff2a623567e2a40ee90f11233
root <root@localhost.localdomain>
2018-07-23 20:59:45 +0100

    Added firewall rule for Jenkins - 23/07/2018

--- build.bash
+++ build.bash
@@ -72,6 +72,10 @@ install_jenkins() {
     wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
     rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
     yum install -y jenkins
+    firewall-cmd --zone=public --permanent --add-port=8080/tcp
+    systemctl restart firewalld
+    systemctl start jenkins
+
 }
 
 # Docker Install
@@ -109,15 +113,15 @@ build_stack() {
 
 # Build Steps
 set_env
-update_system
-setup_folders
-install_java
-install_maven
-setup_users
+#update_system
+#setup_folders
+#install_java
+#install_maven
+#setup_users
 install_jenkins
-docker_install
-docker_compose_install
-docker_machine_install
+#docker_install
+#docker_compose_install
+#docker_machine_install
 #build_stack
 echo "All Done !!"
 # End
